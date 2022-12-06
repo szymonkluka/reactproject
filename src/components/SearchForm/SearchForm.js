b5c8a4c1329f464bf6814c28845f5490ca3dcf7b
Szymon Kluka <szymonkluka@onet.pl>
2022-12-06 20:56:32 +0100

    new commit3

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,9 +1,6 @@
 import styles from './SearchForm.module.scss';
-
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
-
-
 
 const SearchForm = () => {
     return (
